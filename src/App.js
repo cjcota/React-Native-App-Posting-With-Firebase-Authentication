9f1425d904b6e5eaf366b16dc4327248639bd5e2
cjcota <cjcota136@icloud.com>
2019-02-06 15:55:10 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,9 @@ import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import { Provider } from 'react-redux';
 import { connect } from 'react-redux';
-import { exampleIncrement } from './actions'
+import { helloAction } from './actions'
 class App extends React.Component {
   render() {
-    const { helloText } = this.props.second;
     return (
       <View style={styles.container}>
         <Text>{this.props.exampleVar}</Text>
@@ -31,6 +30,9 @@ function mapStateToProps(state) {
 }
 const mapDispatchToProps = dispatch => {
   return {
+    hello: () => {
+      dispatch(helloAction())
+    }
     
   }
 }
