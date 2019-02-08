16bac5699cad4165a2e62ad477bf951a928afd5c
Damon Benz <benzdj11@uww.edu>
2019-02-08 17:55:50 -0600

    hello

--- src/reducers/exampleReducer.js
+++ src/reducers/exampleReducer.js
@@ -3,7 +3,7 @@ export default (state = 0, action) => {
 
   switch(action.type) {
     case 'INCREMENT':
-      return state + 10;
+      return state + 5;
     case SET_COUNTER:
       return action.payload;
     default:
